ac3d24b278a2a201ad487098f929d77634f82040
kongdaqing <zhanghaiyang@zerozero.cn>
2019-09-03 17:54:19 +0800

    add catkin_ws for vins_stereo

--- vins-stereo/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kdq/Workspace/KDQ_SLAM/vins-stereo/src
-# Build directory: /home/kdq/Workspace/KDQ_SLAM/vins-stereo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
