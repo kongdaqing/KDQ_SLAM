dde62c2b52ee681a9e2633a5d852bb2e78803f74
zhangxing <zhangxing@zerozero.cn>
2019-10-11 11:13:51 +0800

    remove catkin_ws

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kdq/workspace/KDQ_SLAM/catkin_ws/src
-# Build directory: /home/kdq/workspace/KDQ_SLAM/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(vins_stereo)
--- catkin_ws/build/vins_stereo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kdq/workspace/KDQ_SLAM/catkin_ws/src/vins_stereo
-# Build directory: /home/kdq/workspace/KDQ_SLAM/catkin_ws/build/vins_stereo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
